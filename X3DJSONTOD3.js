70e6a2cb61d1600ca60404765a5c3fdbbb945940
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-03-09 12:57:21 -0500

    Updated per new X3D JSON spec

--- X3DJSONTOD3.js
+++ X3DJSONTOD3.js
@@ -6,10 +6,21 @@ var content = '';
 process.stdin.resume();
 process.stdin.on('data', function(buf) { content += buf.toString(); });
 
+
+function ConvertChildren(object, indent, parentkey) {
+	var key;
+	for (key in object) {
+		if (typeof object[key] === 'object') {
+				ConvertToD3js(object[key], indent+"\t", key);
+		}
+	}
+}
+
 function ConvertToD3js(object, indent, parentkey) {
 	var key;
 	var localArray = [];
 	var isArray = false;
+	var arrayOfStrings = false;
 	for (key in object) {
 		if (isNaN(parseInt(key))) {
 			isArray = false;
@@ -21,6 +32,7 @@ function ConvertToD3js(object, indent, parentkey) {
 				localArray.push(object[key]);
 			} else if (typeof object[key] === 'string') {
 				localArray.push(object[key]);
+				arrayOfStrings = true;
 			} else if (typeof object[key] === 'boolean') {
 				localArray.push(object[key]);
 			} else if (typeof object[key] === 'object') {
@@ -31,6 +43,8 @@ function ConvertToD3js(object, indent, parentkey) {
 		} else if (typeof object[key] === 'object') {
 			if (key.substr(0,1) === '@') {
 				ConvertToD3js(object[key], indent+"\t", key);
+			} else if (key.substr(0,1) === '-') {
+				ConvertChildren(object[key], indent+"\t", key);
 			} else {
 				console.log(indent+"\telement = element.append('"+key+"');");
 				ConvertToD3js(object[key], indent+"\t", key);
@@ -48,7 +62,12 @@ function ConvertToD3js(object, indent, parentkey) {
 	}
 	if (isArray) {
 		if (parentkey.substr(0,1) === '@') {
-			console.log(indent+"element.attr('"+parentkey.substr(1)+"','"+localArray.join(" ")+"');");
+			if (arrayOfStrings) {
+				arrayOfStrings = false;
+				console.log(indent+"element.attr('"+parentkey.substr(1)+"','\""+localArray.join('" "')+"\"');");
+			} else {
+				console.log(indent+"element.attr('"+parentkey.substr(1)+"','"+localArray.join(" ")+"');");
+			}
 		}
 		isArray = false;
 	}
@@ -58,7 +77,7 @@ process.stdin.on('end', function() {
 	var json = JSON.parse(content);
         console.log('<script type="text/javascript" src="http://www.x3dom.org/download/dev/x3dom-full.js"></script>');
         console.log('<script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>');
-        console.log("<script type='text/javascript'>var element = d3.select('body').append('X3d').attr('id','x3dElement').attr('showLog', 'true').attr('style','width:100%; height:100%; border:0; margin:0; padding:0;').append('Scene');");
+        console.log("<script type='text/javascript'>var element = d3.select('body');");
 
 	ConvertToD3js(json, "", "");
         console.log(";\n</script>");
--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ mkdir -p ${JSON2D3_HOME}/output
 DIRFILE=${JSON2D3_HOME}/output/`basename ${FILE} .x3d`
 
 ${JAVA_HOME}/bin/javac -cp ${JSON2D3_HOME}:${JSON2D3_HOME}/gson-2.3.1.jar ${JSON2D3_HOME}/ParseXML.java ${JSON2D3_HOME}/D3Input.java
-${JAVA_HOME}/bin/java -cp ${JSON2D3_HOME}:${JSON2D3_HOME}/gson-2.3.1.jar ParseXML < "$FILE" > "$DIRFILE".json
+# ${JAVA_HOME}/bin/java -cp ${JSON2D3_HOME}:${JSON2D3_HOME}/gson-2.3.1.jar ParseXML < "$FILE" > "$DIRFILE".json
 ${JAVA_HOME}/bin/java -cp ${JSON2D3_HOME}:${JSON2D3_HOME}/gson-2.3.1.jar D3Input < "$FILE" > "$DIRFILE".radial
 echo "Creating JavaScript..."
-node ${JSON2D3_HOME}/JSON2D3.js < "$DIRFILE".json > "$DIRFILE".js
-(echo '<!doctype html><html><head><link rel="stylesheet" type="text/css" href="http://www.x3dom.org/download/dev/x3dom.css"/></head><body></body>'; cat "$DIRFILE".json | node ${JSON2D3_HOME}/JSON2D3.js; echo  '</html>'; ) > "$DIRFILE".2.html
+#node ${JSON2D3_HOME}/JSON2D3.js < "$DIRFILE".json > "$DIRFILE".js
+#(echo '<!doctype html><html><head><link rel="stylesheet" type="text/css" href="http://www.x3dom.org/download/dev/x3dom.css"/></head><body></body>'; cat "$DIRFILE".json | node ${JSON2D3_HOME}/JSON2D3.js; echo  '</html>'; ) > "$DIRFILE".2.html
+(echo '<!doctype html><html><head><link rel="stylesheet" type="text/css" href="http://www.x3dom.org/download/dev/x3dom.css"/></head><body></body>'; cat "$DIRFILE".json | node ${JSON2D3_HOME}/X3DJSONTOD3.js; echo  '</html>'; ) > "$DIRFILE".3.html
