a8b26f706fea10d6467ef2c1543d0371cb076eee
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-03-26 21:19:58 -0500

    Now handle DTD and default values from DTD (ignore)

--- JWCSON2X3D.js
+++ JWCSON2X3D.js
@@ -12,6 +12,11 @@ function ConvertJSONToX3D(prototypes, indent) {
 	var script = false;
 	var attr;
 	var buffer = '';
+	var firstLevel = false;
+	if (typeof indent === 'undefined') {
+		firstLevel = true;
+		indent = "";
+	}
 	if (typeof prototypes === "object") {
 		for (p in prototypes) {
 			if (typeof prototypes[p] === 'object') {
@@ -22,16 +27,27 @@ function ConvertJSONToX3D(prototypes, indent) {
 					} else if (tag === 'script') {
 						script = true;
 					}
-					console.log(indent+"element = element.append('"+tag+"');");
+					var attrs = [];
 					for (attr in prototypes[p]) {
-						if (attr.indexOf("xmlns:") === 0) {
-							console.log(indent+"d3.ns.qualify('"+attr+"');");
-						} else if (attr !== "jsontag") {
-							console.log(indent+"element.attr('"+attr+"', '"+prototypes[p][attr]+"');");
+						if (attr !== "jsontag") {
+							attrs.push(" "+attr+"='"+prototypes[p][attr]+"'");
 						}
 					}
+					var trailing = '>';
+					if (prototypes.length == 1) {
+						trailing = '/>';
+					}
+					if (attrs.length > 0) {
+						console.log(indent+"<"+tag+attrs.join('')+trailing);
+					} else {
+						console.log(indent+"<"+tag+trailing);
+					}
 				} else {
-					ConvertJSONToX3D(prototypes[p], indent+"\t");
+					if (firstLevel) {
+						ConvertJSONToX3D(prototypes[p], indent);
+					} else {
+						ConvertJSONToX3D(prototypes[p], indent+"  ");
+					}
 				}
 			} else {
 				if (script) {
@@ -39,25 +55,25 @@ function ConvertJSONToX3D(prototypes, indent) {
 				} else if (style) {
 					buffer += prototypes[p];
 				} else {
-					console.log(indent+"/*"+prototypes[p]+"*/");
+					console.log(indent+"  <!--"+prototypes[p]+"-->");
 				}
 			}
 		}
 		if (buffer !== '') {
-			console.log(indent+"element.text('"+buffer
-				.replace(/\n/g, '\\n')
+			console.log(indent+buffer
 				.replace(/&/g, '&amp;')
 				.replace(/</g, '&lt;')
-				.replace(/>/g, '&gt;')+"');");
+				.replace(/>/g, '&gt;'));
 		}
-		console.log(indent+"element = element.select(function () { return this.parentNode;});");
+		if (prototypes.length > 1 && typeof prototypes[0]['jsontag'] !== 'undefined') {
+			console.log(indent+"</"+prototypes[0]['jsontag']+">");
+		}
 	}
 }
 
 process.stdin.on('end', function() {
 	var prototypes = JSON.parse(content);
 console.log('<?xml version="1.0" encoding="UTF-8"?>');
-console.log('<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.3//EN" "http://www.web3d.org/specifications/x3d-3.3.dtd"><X3D><Scene>');
-	ConvertJSONToX3D(prototypes, "");
-console.log('</Scene></X3D>');
+console.log('<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.3//EN" "http://www.web3d.org/specifications/x3d-3.3.dtd">');
+	ConvertJSONToX3D(prototypes, undefined);
 });
--- ParseXML.java
+++ ParseXML.java
@@ -1,4 +1,5 @@
 import org.xml.sax.Attributes;
+import org.xml.sax.ext.Attributes2;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.ext.DefaultHandler2;
@@ -11,6 +12,7 @@ import com.google.gson.stream.JsonWriter;
 class SaxHandler extends DefaultHandler2 {
 
     private JsonWriter writer = null;
+    private boolean dtd = false;
 
     public SaxHandler(JsonWriter writer) {
 	this.writer = writer;
@@ -37,7 +39,9 @@ class SaxHandler extends DefaultHandler2 {
 		writer.beginObject();
 		writer.name("jsontag").value(qName);
 		for (int at = 0; at < attributes.getLength(); at++) {
-			writer.name(attributes.getQName(at)).value(attributes.getValue(at));
+			if (((Attributes2)attributes).isSpecified(at)) {
+				writer.name(attributes.getLocalName(at)).value(attributes.getValue(at));
+			}
 		}
 		writer.endObject();
 	} catch (IOException e) {
@@ -57,12 +61,14 @@ class SaxHandler extends DefaultHandler2 {
     public void comment(char ch[], int start, int length)
         throws SAXException {
 
-	String value = new String(ch, start, length);
-        if(value.length() == 0) return; // ignore white space
-	try {
-		writer.value(value);
-	} catch (IOException e) {
-		e.printStackTrace();
+	if (!dtd) {
+		String value = new String(ch, start, length);
+		if(value.length() == 0) return; // ignore white space
+		try {
+			writer.value(value);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
     }
     public void characters(char ch[], int start, int length)
@@ -76,6 +82,18 @@ class SaxHandler extends DefaultHandler2 {
 		e.printStackTrace();
 	}
     }
+    public void startDTD(String name,
+                     String publicId,
+                     String systemId)
+              throws SAXException {
+	dtd = true;
+    }
+
+    public void endDTD()
+            throws SAXException {
+	dtd = false;
+    }
+
 }    
 
 public class ParseXML {
