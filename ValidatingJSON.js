7f44087f269fbadcb2f4eb997f3dc1cf614b2732
John Carlson <johncarlson@Johns-MacBook-Air.local>
2015-05-06 15:03:20 -0500

    added JSONLD.html.   Provided script for validating JSON files on the command line.

--- ValidatingJSON.js
+++ ValidatingJSON.js
@@ -1,12 +1,24 @@
 // Java Style Sheet Language, implementation 2
 
-var content = '';
-// read content into buffer
-process.stdin.resume();
-process.stdin.on('data', function(buf) { content += buf.toString(); });
+var fs = require('fs');
 
-
-process.stdin.on('end', function() {
-	var prototypes = JSON.parse(content);
-	console.log(JSON.stringify(prototypes, null, "\t"));
-});
+function parseFile(file) {
+	fs.readFile(file, function(err, data) {
+		if (err) {
+			throw err;
+		}
+		try {
+			JSON.parse(data);
+			console.log(file, 'succeeded');
+		} catch (e) {
+			console.log(file, 'failed', e);
+		}
+	});
+}
+for (i in process.argv) {
+	if (i < 2) {
+		continue;
+	}
+	var file = process.argv[i];
+	parseFile(file);
+}
---
+++ validate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+node ValidatingJSON.js `find ~/Downloads/www.web3d.org/x3d/content/examples/X3dForWebAuthors/ -name '*json'`
